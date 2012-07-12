d3b109ffdb1a6400bc78d103695d86ddff0f53c1
manne ohrstrom <manne@shotgunsoftware.com>
2012-07-12 17:40:41 +0100

    re-aligning logo.

--- python/tk_nuke/ui/dialog.py
+++ python/tk_nuke/ui/dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialog.ui'
 #
-# Created: Thu Jul 12 12:15:48 2012
+# Created: Thu Jul 12 17:39:46 2012
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
