72fd354a36a9e21bab72db0651e63c45ac8b30ec
manne ohrstrom <manne@shotgunsoftware.com>
2013-02-03 12:32:00 +0000

    removed old necessary folder check.

--- engine.py
+++ engine.py
@@ -58,25 +58,11 @@ class NukeEngine(tank.platform.Engine):
         self.log_debug("%s: Initializing..." % self)
 
         # now check that there is a location on disk which corresponds to the context
-        if self.context.entity:
-            # context has an entity
-            locations = self.tank.paths_from_entity(self.context.entity["type"], 
-                                                    self.context.entity["id"])
-        elif self.context.project:
-            # context has a project
-            locations = self.tank.paths_from_entity(self.context.project["type"], 
-                                                    self.context.project["id"])
-        else:
+        if self.context.project is None:
             # must have at least a project in the context to even start!
             raise tank.TankError("The nuke engine needs at least a project in the context "
                                  "in order to start! Your context: %s" % self.context)
-        
-        # make sure there are folders on disk
-        if len(locations) == 0:
-            raise tank.TankError("No folders on disk are associated with the current context. The Nuke "
-                            "engine requires a context which exists on disk in order to run "
-                            "correctly.")
-        
+                
         # make sure we are not running that bloody nuke PLE!
         if nuke.env.get("ple") == True:
             self.log_error("The Nuke Engine does not work with the Nuke PLE!")
