873cfc58396fe8a5478890eed6c286c8e06e7a33
Jeff Beeland <jeff.beeland@autodesk.com>
2015-12-14 12:13:59 -0800

    Replaces tabs with spaces (Sublime...sigh) and explicitly stringifies exceptions when catching and re-raising as TankErrors.

--- python/startup/menu.py
+++ python/startup/menu.py
@@ -11,53 +11,53 @@
 import os
 
 def bootstrap_sgtk():
-	"""
-	Bootstrapping routine for the Nuke mode of Nuke.
-	"""
-	import nuke
+    """
+    Bootstrapping routine for the Nuke mode of Nuke.
+    """
+    import nuke
 
-	_setup_sgtk(nuke.warning)
+    _setup_sgtk(nuke.warning)
 
-	# Clean up temp env vars.
-	_clean_env()
+    # Clean up temp env vars.
+    _clean_env()
 
 def _clean_env():
-	"""
-	Cleans up SGTK related environment variables.
-	"""
-	for var in ["TANK_ENGINE", "TANK_CONTEXT", "TANK_FILE_TO_OPEN"]:
-		if var in os.environ:
-			del os.environ[var]
+    """
+    Cleans up SGTK related environment variables.
+    """
+    for var in ["TANK_ENGINE", "TANK_CONTEXT", "TANK_FILE_TO_OPEN"]:
+        if var in os.environ:
+            del os.environ[var]
 
 def _setup_sgtk(output_handle):
-	"""
-	Extracts the necessary information from the environment and starts
-	the tk-nuke engine.
-	"""
-	try:
-		import tank
-	except Exception, e:
-		output_handle("Shotgun: Could not import sgtk! Disabling: %s" % e)
-		return
+    """
+    Extracts the necessary information from the environment and starts
+    the tk-nuke engine.
+    """
+    try:
+        import tank
+    except Exception, e:
+        output_handle("Shotgun: Could not import sgtk! Disabling: %s" % str(e))
+        return
 
-	if not "TANK_ENGINE" in os.environ:
-		output_handle("Shotgun: Unable to determine engine to start!")
-		return
+    if not "TANK_ENGINE" in os.environ:
+        output_handle("Shotgun: Unable to determine engine to start!")
+        return
 
-	engine_name = os.environ.get("TANK_ENGINE")
-	try:
-		context = tank.context.deserialize(os.environ.get("TANK_CONTEXT"))
-	except Exception, e:
-		output_handle(
-			"Shotgun: Could not create context! "
-			"Shotgun Toolkit will be disabled. Details: %s" % e
-		)
-		return
+    engine_name = os.environ.get("TANK_ENGINE")
+    try:
+        context = tank.context.deserialize(os.environ.get("TANK_CONTEXT"))
+    except Exception, e:
+        output_handle(
+            "Shotgun: Could not create context! "
+            "Shotgun Toolkit will be disabled. Details: %s" % str(e)
+        )
+        return
 
-	try:
-		engine = tank.platform.start_engine(engine_name, context.tank, context)
-	except Exception, e:
-		output_handle("Shotgun: Could not start engine: %s" % e)
-		return
+    try:
+        engine = tank.platform.start_engine(engine_name, context.tank, context)
+    except Exception, e:
+        output_handle("Shotgun: Could not start engine: %s" % str(e))
+        return
 
 bootstrap_sgtk()
