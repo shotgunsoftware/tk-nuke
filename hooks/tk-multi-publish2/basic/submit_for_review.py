2a83e1ee1aedd6cfea8be7ac486509c9d191677c
Philip Scadding <philip.scadding@autodesk.com>
2018-05-01 20:12:04 +0100

    removed erroneous character from description

--- hooks/tk-multi-publish2/basic/submit_for_review.py
+++ hooks/tk-multi-publish2/basic/submit_for_review.py
@@ -52,7 +52,7 @@ class NukeSubmitForReviewPlugin(HookBaseClass):
         review_url = "https://support.shotgunsoftware.com/hc/en-us/articles/114094032014-The-review-workflow"
 
         return """<p>
-        Submits a movie file to Shotgun for review. An entry will beß
+        Submits a movie file to Shotgun for review. An entry will be
         created in Shotgun which will include a reference to the movie file's current
         path on disk. Other users will be able to access the file via
         the <b><a href='%s'>review app</a></b> on the Shotgun website.</p>
