4ed766e75154654b8596716d9d4142ea5f94c0e3
Ariel Calzada <ariel.calzada.solano@autodesk.com>
2022-11-03 08:24:59 -0500

    Change message title

--- engine.py
+++ engine.py
@@ -294,7 +294,7 @@ class NukeEngine(sgtk.platform.Engine):
                 """
                 self._QtGui.QMessageBox.critical(
                     self._dialog,
-                    "Warning",
+                    "Your ShotGrid session has expired",
                     "The ShotGrid session has expired, to continue using "
                     "ShotGrid in Nuke, please restart Nuke.",
                     self._QtGui.QMessageBox.Ok,
