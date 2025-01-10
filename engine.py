9d199ce59548dc1ff327c3f8dd0fe391e138e177
Patrick Macdonald <4705216+reformstudios@users.noreply.github.com>
2025-01-10 12:14:54 +0000

    Update engine.py (#79)
    It's probably bad practice to permanently change hiero's log level just to print a sg debug log.
    HieroPlayer doesn't have a frameserver, so the warning in this file fires every second : /apps/foundry/nuke/Nuke12.2v4/pythonextensions/site-packages/hiero/ui/FnStatusBar.py
    To prevent this, we need to revert hiero's log level to what it was before we emit a debug level message.

--- engine.py
+++ engine.py
@@ -698,6 +698,8 @@ class NukeEngine(sgtk.platform.Engine):
         if self.hiero_enabled:
             import hiero
 
+            existing_log_level = hiero.core.log.logLevel()
+
             if record.levelno >= logging.ERROR:
                 hiero.core.log.error(msg)
             elif record.levelno >= logging.WARNING:
@@ -707,6 +709,7 @@ class NukeEngine(sgtk.platform.Engine):
             else:
                 hiero.core.log.setLogLevel(hiero.core.log.kDebug)
                 hiero.core.log.debug(msg)
+                hiero.core.log.setLogLevel(existing_log_level)
         else:
             if record.levelno >= logging.CRITICAL:
                 nuke.critical("PTR Critical: %s" % msg)
