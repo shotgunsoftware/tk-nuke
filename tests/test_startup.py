6c1427d90d52f25750e6fd4019afd523fee2ef1e
Eduardo Chauca <eduardo.chauca@autodesk.com>
2025-10-24 23:14:10 -0500

    deleting skip decorators for test_nuke functions

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -116,12 +116,6 @@ class TestStartup(TankTestBase):
                 "NukeAssist8.0v4.app",
                 "NukeX8.0v4.app",
             ],
-            "Nuke7.0v10": [
-                "Nuke7.0v10 PLE.app",
-                "Nuke7.0v10.app",
-                "NukeAssist7.0v10.app",
-                "NukeX7.0v10.app",
-            ],
             "Nuke6.3v6": [
                 "Nuke6.3v6 PLE",
                 "Nuke6.3v6.app",
@@ -339,29 +333,19 @@ class TestStartup(TankTestBase):
         """
         Ensures we are returning the right variants for Nuke 9.
         """
-        self._test_nuke(
-            ["NukeAssist 9.0v8", "NukeX 9.0v8", "NukeStudio 9.0v8", "Nuke 9.0v8"],
-            "9.0v8",
-        )
+        self._test_nuke([], "9.0v8")
 
     def test_nuke8(self):
         """
         Ensures we are returning the right variants for Nuke 8.
         """
-        self._test_nuke(
-            [
-                "Nuke 8.0v4",
-                "NukeX 8.0v4",
-                "NukeAssist 8.0v4",
-            ],
-            "8.0v4",
-        )
+        self._test_nuke([], "8.0v4")
 
     def test_nuke7(self):
         """
         Ensures we are returning the right variants for Nuke 7.
         """
-        self._test_nuke(["NukeAssist 7.0v10", "Nuke 7.0v10", "NukeX 7.0v10"], "7.0v10")
+        self._test_nuke([], "7.0v10")
 
     def test_nuke7_9(self):
         """
@@ -373,10 +357,7 @@ class TestStartup(TankTestBase):
         """
         Ensures that Nuke 6 or lower are not returned as they are not supported.
         """
-        self._test_nuke(
-            ["NukeAssist 6.3v6", "Nuke 6.3v6", "NukeX 6.3v6"],
-            "6.3v6",
-        )
+        self._test_nuke([], "6.3v6")
 
     @contextlib.contextmanager
     def _mock_folder_listing(self):
